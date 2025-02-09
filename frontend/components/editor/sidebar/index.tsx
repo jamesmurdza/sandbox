f257cdb369ab1f0b80266bf257b69b4c4072dde5
Akhileshrangani4 <akhileshrangani4@gmail.com>
2025-02-09 12:11:44 -0500

    fix: resolve typescript error

--- frontend/components/editor/sidebar/index.tsx
+++ frontend/components/editor/sidebar/index.tsx
@@ -165,7 +165,7 @@ export default function Sidebar({
                   />
                 )
               )}
-              {creatingNew !== null ? (
+              {creatingNew !== null && socket ? (
                 <New
                   socket={socket}
                   type={creatingNew}
