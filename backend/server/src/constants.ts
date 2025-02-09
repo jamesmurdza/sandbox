f9670931377ea57e78e600757c07fec6589eb05d
James Murdza <james@jamesmurdza.com>
2025-02-09 14:51:39 -0600

    fix: increase the margin between pausing a container and timeout

--- backend/server/src/constants.ts
+++ backend/server/src/constants.ts
@@ -1,3 +1,3 @@
 // The amount of time in ms that a container will stay alive without a hearbeat.
-export const CONTAINER_TIMEOUT = 120_000
-export const CONTAINER_PAUSE = 115_000
+export const CONTAINER_TIMEOUT = 3 * 60_000
+export const CONTAINER_PAUSE = 2 * 60_000
