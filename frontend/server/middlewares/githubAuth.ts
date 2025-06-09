be9dfd4c6fade1b37f50de34c075da62e265f91f
Hamzat Victor <oluwaborihamzat@gmail.com>
2025-06-09 19:36:59 +0200

    feat: add GitHub authentication middleware and complete all GitHub integration routes

--- frontend/server/middlewares/githubAuth.ts
+++ frontend/server/middlewares/githubAuth.ts
@@ -1,14 +1,35 @@
+import { GitHubManager } from "@/lib/github"
 import { AppBindings } from "@/lib/server/types"
 import { createMiddleware } from "hono/factory"
 import { db } from "../db"
 
-export const githubAuth = createMiddleware<AppBindings>(async (c, next) => {
-  const clerkUser = c.get("user")
-  const dbUser = await db.query.user.findFirst({
-    where: (user, { eq }) => eq(user.id, clerkUser.id),
-  })
-  if (!dbUser?.githubToken) {
-    return c.json({ message: "GitHub authentication required" }, 403)
+export interface GithubAppBindings extends AppBindings {
+  Variables: AppBindings["Variables"] & {
+    manager: GitHubManager
   }
-  next()
-})
+}
+export const githubAuth = createMiddleware<GithubAppBindings>(
+  async (c, next) => {
+    const clerkUser = c.get("user")
+    const dbUser = await db.query.user.findFirst({
+      where: (user, { eq }) => eq(user.id, clerkUser.id),
+    })
+    if (!dbUser?.githubToken) {
+      return c.json(
+        { success: false, message: "GitHub authentication required" },
+        403
+      )
+    }
+    try {
+      const manager = new GitHubManager({ token: dbUser.githubToken })
+      await manager.init()
+      c.set("manager", manager)
+      return next()
+    } catch (error) {
+      return c.json(
+        { success: false, message: "GitHub authentication failed" },
+        403
+      )
+    }
+  }
+)
