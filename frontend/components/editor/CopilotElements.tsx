0517545af33a14bf01371e24ee855b7d4fb50015
Hamzat Victor <oluwaborihamzat@gmail.com>
2025-06-22 18:29:39 +0200

    refactor: Streamline CopilotElements and Generate components by removing unused props and simplifying structure

--- frontend/components/editor/CopilotElements.tsx
+++ frontend/components/editor/CopilotElements.tsx
@@ -1,24 +1,25 @@
 "use client"
-
-import { User } from "@/lib/types"
-import { cn } from "@/lib/utils"
+import { GenerateState } from "@/components/editor/hooks/useMonacoEditor"
+import { TTab } from "@/lib/types"
+import * as monaco from "monaco-editor"
 import AISuggestionWidget from "./AISuggestionWidget"
-import GenerateInput from "./generate"
+import { GenerateWidget } from "./generate"
+import { useGenerateWidget } from "./hooks/useGenerateWidget"
 
 export interface CopilotElementsProps {
   generateRef: React.RefObject<HTMLDivElement>
-  suggestionWidget: {
-    isSelected: boolean
-    showSuggestion: boolean
-    onAiEdit: () => void
-    suggestionRef: React.RefObject<HTMLDivElement>
-  }
-  generateWidget: {
-    show: boolean
-    generateWidgetRef: React.RefObject<HTMLDivElement>
-    userData: User
-    generateInputProps: any
-  }
+  suggestionRef: React.RefObject<HTMLDivElement>
+  generateWidgetRef: React.RefObject<HTMLDivElement>
+  editorRef: monaco.editor.IStandaloneCodeEditor | undefined
+  generate: GenerateState
+  setGenerate: React.Dispatch<React.SetStateAction<GenerateState>>
+  isSelected: boolean
+  cursorLine: number
+  tabs: TTab[]
+  activeFileId: string
+  editorLanguage: string
+  handleAiEdit: (editor?: monaco.editor.ICodeEditor) => void
+  showSuggestion: boolean
 }
 
 /**
@@ -26,34 +27,48 @@ export interface CopilotElementsProps {
  */
 export default function CopilotElements({
   generateRef,
-  suggestionWidget,
-  generateWidget,
-}: CopilotElementsProps) {
+  generateWidgetRef,
+  suggestionRef,
+  editorRef,
+  generate,
+  setGenerate,
+  isSelected,
+  cursorLine,
+  tabs,
+  activeFileId,
+  editorLanguage,
+  showSuggestion,
+  handleAiEdit,
+}: // suggestionWidget,
+// generateWidget,
+CopilotElementsProps) {
+  const generateInputProps = useGenerateWidget({
+    editorRef,
+    generate,
+    setGenerate,
+    isSelected,
+    cursorLine,
+    generateRef,
+    tabs,
+    activeFileId,
+    editorLanguage,
+  })
   return (
     <>
-      {/* Generate DOM anchor point */}
-      <div ref={generateRef} />
-
+      {/* Generate Widget */}
+      <GenerateWidget
+        generateRef={generateRef}
+        generateWidgetRef={generateWidgetRef}
+        show={generate.show}
+        {...generateInputProps}
+      />
       {/* AI Suggestion Widget */}
       <AISuggestionWidget
-        isSelected={suggestionWidget.isSelected}
-        showSuggestion={suggestionWidget.showSuggestion}
-        onAiEdit={suggestionWidget.onAiEdit}
-        suggestionRef={suggestionWidget.suggestionRef}
+        isSelected={isSelected}
+        showSuggestion={showSuggestion}
+        onAiEdit={handleAiEdit}
+        suggestionRef={suggestionRef}
       />
-
-      {/* Generate Widget */}
-      <div
-        className={cn(generateWidget.show && "z-50 p-1")}
-        ref={generateWidget.generateWidgetRef}
-      >
-        {generateWidget.show ? (
-          <GenerateInput
-            user={generateWidget.userData}
-            {...generateWidget.generateInputProps}
-          />
-        ) : null}
-      </div>
     </>
   )
 }
