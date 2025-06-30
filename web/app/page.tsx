dd4b388a97f62b1ff936c91d404c9dcc5db21e39
abdulrehmann231 <mabdulrehman951@gmail.com>
2025-06-30 11:59:25 -0700

    chore: redirect requests from landing page to dashboard

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,5 @@
-import Landing from "@/components/landing"
-import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 
 export default async function Home() {
-  const user = await currentUser()
-
-  if (user) {
-    redirect("/dashboard")
-  }
-
-  return <Landing />
+  redirect("/dashboard")
 }
