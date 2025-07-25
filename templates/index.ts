102c01b59b5adf7b8868b04abef3498f1dfe6293
James Murdza <james@jamesmurdza.com>
2025-07-25 16:48:41 -0400

    chore: move projectTemplates to templates workspace

--- templates/index.ts
+++ templates/index.ts
@@ -288,3 +288,47 @@ export const templateConfigs: { [key: string]: TemplateConfig } = {
     },
   },
 }
+
+export const projectTemplates: {
+  id: string
+  name: string
+  icon: string
+  description: string
+  disabled: boolean
+}[] = [
+  {
+    id: "reactjs",
+    name: "React",
+    icon: "/project-icons/react.svg",
+    description: "A JavaScript library for building user interfaces",
+    disabled: false,
+  },
+  {
+    id: "vanillajs",
+    name: "HTML/JS",
+    icon: "/project-icons/more.svg",
+    description: "A simple HTML/JS project for building web apps",
+    disabled: false,
+  },
+  {
+    id: "nextjs",
+    name: "NextJS",
+    icon: "/project-icons/next-js.svg",
+    description: "a React framework for building full-stack web applications",
+    disabled: false,
+  },
+  {
+    id: "streamlit",
+    name: "Streamlit",
+    icon: "/project-icons/python.svg",
+    description: "A faster way to build and share data apps",
+    disabled: false,
+  },
+  {
+    id: "php",
+    name: "PHP",
+    description: "PHP development environment",
+    icon: "/project-icons/php.svg",
+    disabled: false,
+  },
+]
